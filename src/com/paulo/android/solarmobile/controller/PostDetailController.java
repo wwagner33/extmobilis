bbb4cdd987283ec59eb7b77eebf9ef69c979e383
Paulo Costa <paulombcosta@gmail.com>
2012-01-30 17:39:39 -0300

    parte da tela de respsta

--- src/com/paulo/android/solarmobile/controller/PostDetailController.java
+++ src/com/paulo/android/solarmobile/controller/PostDetailController.java
@@ -1,13 +1,18 @@
 package com.paulo.android.solarmobile.controller;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class PostDetailController extends Activity {
+public class PostDetailController extends Activity implements OnClickListener {
 
 	TextView body, userName, forumName;
 	Bundle extras;
+	Button response;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,8 @@ public class PostDetailController extends Activity {
 		body = (TextView) findViewById(R.id.post_body_detail);
 		forumName = (TextView) findViewById(R.id.nome_forum_detail);
 		userName = (TextView) findViewById(R.id.user_name_detail);
+		response = (Button)findViewById(R.id.answer_topic);
+		response.setOnClickListener(this);
 
 		extras = getIntent().getExtras();
 		if (extras != null) {
@@ -24,7 +31,16 @@ public class PostDetailController extends Activity {
 			forumName.setText(extras.getString("forumName"));
 			userName.setText(extras.getString("username"));
 		}
+		
+		
+		
 
+	}
+
+	@Override
+	public void onClick(View v) {
+		Intent intent = new Intent(this,ResponseController.class);
+		startActivity(intent);
 	}
 
 }
--- src/com/paulo/android/solarmobile/controller/ResponseController.java
+++ src/com/paulo/android/solarmobile/controller/ResponseController.java
@@ -1,18 +1,70 @@
 package com.paulo.android.solarmobile.controller;
 
-import java.io.File;
+import org.json.simple.JSONObject;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class ResponseController extends Activity {
+import com.paulo.android.solarmobile.ws.Connection;
 
-	File recordingsFolder;
+public class ResponseController extends Activity implements OnClickListener {
+	
+	
+	
+	//Wedson: curl -v -H 'Content-Type: application/json' -H 'Accept: application/json' -X POST 
+	//http://localhost:3000/discussions/1/posts?auth_token=B3BQ1twAooSXWY53hktp --data '{"discussion_post":{"content":"estou criando um novo post dentro de 1", "parent_id":""}}'
+		  
+		//reponder um forum
+	
+	
+	
+
+	// File recordingsFolder;
+	EditText message;
+	Button submit;
+	Connection connection;
+	JSONObject responseJSON;
+	ProgressDialog dialog;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.responder_topico);
+
+		// submit.setOnClickListener(this);
+
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public class SendNewPost extends AsyncTask<Void, Void, Void> {
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			connection = new Connection(getApplicationContext());
+
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			// TODO Auto-generated method stub
+			super.onPostExecute(result);
+
+			Intent intent = new Intent(getApplicationContext(), PostList.class);
+			startActivity(intent);
+		}
 
 	}
 }
