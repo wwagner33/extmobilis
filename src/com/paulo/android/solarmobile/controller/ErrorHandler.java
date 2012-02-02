fd0c6fa766274b95e40c730013b8f9ec08a1474a
Paulo Costa <paulombcosta@gmail.com>
2012-02-02 17:26:13 -0300

    Inserindo mensagem de erros

--- src/com/paulo/android/solarmobile/controller/ErrorHandler.java
+++ src/com/paulo/android/solarmobile/controller/ErrorHandler.java
@@ -13,11 +13,27 @@ public class ErrorHandler {
 		}
 
 		if (errorId == Constants.ERROR_CONNECTION_TIMEOUT) {
-					//TBI
+			Toast.makeText(context, "Tempo limite de conexão atingido",
+					Toast.LENGTH_SHORT).show();
 		}
+
+		if (errorId == Constants.ERROR_SERVER_DOWN) {
+			Toast.makeText(context, "Servidor Indisponível", Toast.LENGTH_SHORT)
+					.show();
+		}
+		if (errorId == Constants.ERROR_TOKEN_EXPIRED) {
+			Toast.makeText(context, "Token expirou", Toast.LENGTH_SHORT).show();
+		}
+
+		if (errorId == Constants.ERROR_UNKNOWN) {
+
+			Toast.makeText(context, "Erro desconhecido", Toast.LENGTH_SHORT)
+					.show();
+		}
+		
 	}
 
-	/*
+	/*	OLD IMPLEMENTATION
 	 * public void handleError(int errorCode) { dialog.dismiss(); if (errorCode
 	 * == Constants.ERROR_CONNECTION_FAILED) { Toast.makeText(this,
 	 * "Erro de conexão,tente novamente ", Toast.LENGTH_SHORT).show();
