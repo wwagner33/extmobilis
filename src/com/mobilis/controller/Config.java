d09317108d9fb9923ee9b66938f0b7d29baa7233
Paulo Costa <paulombcosta@gmail.com>
2012-03-29 11:07:19 -0300

    adicionados tela de configurações e item de acesso no menu

---
+++ src/com/mobilis/controller/Config.java
@@ -0,0 +1,56 @@
+package com.mobilis.controller;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceManager;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceActivity;
+import android.widget.Toast;
+
+public class Config extends PreferenceActivity implements
+		OnPreferenceChangeListener {
+
+	private CheckBoxPreference autoLogin;
+	private SharedPreferences settings;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		settings = PreferenceManager.getDefaultSharedPreferences(this);
+		addPreferencesFromResource(R.layout.config);
+		autoLogin = (CheckBoxPreference) findPreference("checkbox_preference");
+		autoLogin.setOnPreferenceChangeListener(this);
+
+		boolean initialState = (settings.getBoolean("AutoLogin", true)) ? true
+				: false;
+		autoLogin.setChecked(initialState);
+
+	}
+
+	@Override
+	public boolean onPreferenceChange(Preference preference, Object value) {
+		// newValue = (Boolean) newValue;
+		boolean newValue = ((Boolean) value) ? true : false;
+		Toast.makeText(this, String.valueOf(newValue), Toast.LENGTH_SHORT)
+				.show();
+
+		if (newValue == true) {
+
+			SharedPreferences.Editor editor = settings.edit();
+			editor.putBoolean("AutoLogin", newValue);
+			editor.commit();
+			autoLogin.setChecked(true);
+
+		} else {
+			SharedPreferences.Editor editor = settings.edit();
+			editor.putBoolean("AutoLogin", newValue);
+			editor.commit();
+			autoLogin.setChecked(false);
+		}
+
+		return false;
+	}
+}
--- src/com/mobilis/controller/InitialConfig.java
+++ src/com/mobilis/controller/InitialConfig.java
@@ -4,9 +4,10 @@ import java.io.File;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.os.Environment;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 import com.mobilis.model.DBAdapter;
@@ -17,10 +18,13 @@ public class InitialConfig extends Activity {
 	private Cursor cursor;
 	private DBAdapter adapter;
 	private Bundle extras;
+	private SharedPreferences settings;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		settings = PreferenceManager.getDefaultSharedPreferences(this);
 
 		File file = new File(Constants.PATH_MAIN_FOLDER);
 		if (!file.exists()) {
@@ -44,10 +48,20 @@ public class InitialConfig extends Activity {
 				adapter.open();
 
 				if (adapter.tokenExists()) {
-					Log.w("TOKEN TESTE", "TOKEN OK");
-					intent = new Intent(this, CourseListController.class);
-					startActivity(intent);
+
+					if (settings.getBoolean("AutoLogin", true)) {
+						Log.w("TOKEN TESTE", "TOKEN OK");
+						intent = new Intent(this, CourseListController.class);
+						startActivity(intent);
+					} else {
+						Log.w("AutoLogin", "FALSE");
+						intent = new Intent(this, Login.class);
+						startActivity(intent);
+
+					}
 				} else {
+
+					setConfigurations();
 					Log.w("TOKEN TESTE", "NO TOKEN");
 					Intent intent = new Intent(this, Login.class);
 
@@ -62,7 +76,17 @@ public class InitialConfig extends Activity {
 				intent = new Intent(this, Login.class);
 				startActivity(intent);
 			}
+
 		}
+
+	}
+
+	public void setConfigurations() {
+		// setting default preferences
+		SharedPreferences.Editor editor = settings.edit();
+		editor.putBoolean("AutoLogin", true);
+		editor.commit();
+
 	}
 
 	@Override
