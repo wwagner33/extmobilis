584a20314c14a2d87f617e054295f5ad67947019
LuizQueiroz <luizbarrosojr@gmail.com>
2013-07-11 16:50:54 -0300

    [#53176959] Login de teste

--- SolarMobilis/src/com/example/solarmobilis/LoginActivity.java
+++ SolarMobilis/src/com/example/solarmobilis/LoginActivity.java
@@ -1,16 +1,28 @@
 package com.example.solarmobilis;
 
-import com.googlecode.androidannotations.annotations.EActivity;
-
-import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.EditText;
+import android.widget.Toast;
 
-
+import com.googlecode.androidannotations.annotations.Click;
+import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.activity_login)
 public class LoginActivity extends Activity {
 
+	@ViewById(R.id.editText1)
+	EditText usuario;
+	@ViewById(R.id.editText2)
+	EditText senha;
+
+	@Click(R.id.button1)
+	void submit(){
+		if (!(usuario.getText().toString().trim().length() == 0 || senha.getText().toString().trim().length() == 0)){
+			Toast.makeText(LoginActivity.this, "bem vindo: "+ usuario.getText().toString() +" senha: " + senha.getText().toString(), Toast.LENGTH_LONG).show();
+		}
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
