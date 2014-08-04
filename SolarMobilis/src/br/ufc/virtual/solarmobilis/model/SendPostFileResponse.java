5b7092205a77a3080227ecdbc7f1b20aec0d72cc
Humberto Gomes <humberto@virtual.ufc.br>
2014-08-04 08:53:24 -0300

    [#75778410] Envio de anexos de post utilizando AA. Arquivo que faltou no commit anterior.

---
+++ SolarMobilis/src/br/ufc/virtual/solarmobilis/model/SendPostFileResponse.java
@@ -0,0 +1,16 @@
+package br.ufc.virtual.solarmobilis.model;
+
+import java.util.List;
+
+public class SendPostFileResponse {
+	public List<Integer> ids;
+
+	public List<Integer> getIds() {
+		return ids;
+	}
+
+	public void setIds(List<Integer> ids) {
+		this.ids = ids;
+	}
+
+}
